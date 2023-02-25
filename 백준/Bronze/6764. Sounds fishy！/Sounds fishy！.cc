67f5c7058805cb291479f350c41dcce3a17f161b
in-jun <me@injun.dev>
2023-02-25 12:59:03 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (a < b && b < c && c < d)
+        cout << "Fish Rising";
+    else if (a > b && b > c && c > d)
+        cout << "Fish Diving";
+    else if (a == b && b == c && c == d)
+        cout << "Fish At Constant Depth";
+    else
+        cout << "No Fish";
+    return 0;
+}
