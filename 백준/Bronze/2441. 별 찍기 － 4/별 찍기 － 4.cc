d9c1dab9f246f302dbeccdb15f669d0fd1db1336
인준 <115498126+in-jun@users.noreply.github.com>
2023-02-25 13:51:47 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+            cout << ' ';
+        for (int j = 0; j < n - i; j++)
+            cout << '*';
+        cout << '\n';
+    }
+    return 0;
+}
