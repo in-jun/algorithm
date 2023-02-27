d8a217037260b1b3f3fcf47ce49a6a61b778f0fe
인준 <115498126+in-jun@users.noreply.github.com>
2023-02-27 23:05:17 +0900

    [Bronze IV] Title: Archivist, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.cc
@@ -0,0 +1,87 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int y;
+    cin >> y;
+    switch (y)
+    {
+    case 1995:
+        cout << "ITMO";
+        break;
+    case 1996:
+        cout << "SPbSU";
+        break;
+    case 1997:
+        cout << "SPbSU";
+        break;
+    case 1998:
+        cout << "ITMO";
+        break;
+    case 1999:
+        cout << "ITMO";
+        break;
+    case 2000:
+        cout << "SPbSU";
+        break;
+    case 2001:
+        cout << "ITMO";
+        break;
+    case 2002:
+        cout << "ITMO";
+        break;
+    case 2003:
+        cout << "ITMO";
+        break;
+    case 2004:
+        cout << "ITMO";
+        break;
+    case 2005:
+        cout << "ITMO";
+        break;
+    case 2006:
+        cout << "PetrSU, ITMO";
+        break;
+    case 2007:
+        cout << "SPbSU";
+        break;
+    case 2008:
+        cout << "SPbSU";
+        break;
+    case 2009:
+        cout << "ITMO";
+        break;
+    case 2010:
+        cout << "ITMO";
+        break;
+    case 2011:
+        cout << "ITMO";
+        break;
+    case 2012:
+        cout << "ITMO";
+        break;
+    case 2013:
+        cout << "SPbSU";
+        break;
+    case 2014:
+        cout << "ITMO";
+        break;
+    case 2015:
+        cout << "ITMO";
+        break;
+    case 2016:
+        cout << "ITMO";
+        break;
+    case 2017:
+        cout << "ITMO";
+        break;
+    case 2018:
+        cout << "SPbSU";
+        break;
+    case 2019:
+        cout << "ITMO";
+    }
+    return 0;
+}
