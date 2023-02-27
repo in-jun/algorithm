3672e206d37ba3200a45b56697475e2160349504
인준 <115498126+in-jun@users.noreply.github.com>
2023-02-27 22:42:53 +0900

    [Bronze IV] Title: ICPC, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16727. ICPC/ICPC.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int p1, s1, s2, p2;
+    cin >> p1 >> s1 >> s2 >> p2;
+    if ((p1 + p2) == (s1 + s2))
+    {
+        if (p1 == s2)
+            cout << ("Penalty");
+        else if (p1 > s2)
+            cout << ("Esteghlal");
+        else
+            cout << ("Persepolis");
+    }
+    else if ((p1 + p2) > (s1 + s2))
+        cout << "Persepolis";
+    else
+        cout << "Esteghlal";
+    return 0;
+}
