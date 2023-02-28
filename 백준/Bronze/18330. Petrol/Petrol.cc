c6e117220e19eea337af882596c6a29a8b74fcaa
인준 <115498126+in-jun@users.noreply.github.com>
2023-02-28 19:15:44 +0900

    [Bronze IV] Title: Petrol, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18330. Petrol/Petrol.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    long int n, k;
+    cin >> n >> k;
+    cout << ((n <= (60 + k)) ? n * 1500 : (((60 + k) * 1500) + ((n - (60 + k)) * 3000)));
+    return 0;
+}
