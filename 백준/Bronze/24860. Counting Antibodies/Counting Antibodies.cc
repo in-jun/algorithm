745bb26a2b18c7ef4239135613fa87627a47b5b1
인준 <115498126+in-jun@users.noreply.github.com>
2023-03-01 15:45:47 +0900

    [Bronze IV] Title: Counting Antibodies, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24860. Counting Antibodies/Counting Antibodies.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    long long int vk, jk, va, ja, vh, dh, jh;
+    cin >> vk >> jk >> va >> ja >> vh >> dh >> jh;
+    cout<<(vh * dh * jh)*((vk * jk)+(va * ja));
+    return 0;
+}
