07e7632f8b87f92bddcac45baa0f5a4f184a34bd
인준 <115498126+in-jun@users.noreply.github.com>
2023-03-08 23:47:12 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using std::cin;
+using std::cout;
+using std::sort;
+
+int main()
+{
+    int n, arr[1000];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr, arr + n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << '\n';
+    return 0;
+}
