456ab667d88580b6582e8f55e601021f5f32c77c
in-jun <me@injun.dev>
2023-03-10 22:51:12 +0900

    [Bronze IV] Title: Rook, Time: 12 ms, Memory: 70348 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/README.md
@@ -0,0 +1,24 @@
+# [Bronze IV] Rook - 2393 
+
+[문제 링크](https://www.acmicpc.net/problem/2393) 
+
+### 성능 요약
+
+메모리: 70348 KB, 시간: 12 ms
+
+### 분류
+
+구현(implementation)
+
+### 문제 설명
+
+<p>You have just learned how to output text to the screen and your teacher has challenged you to create an ASCII art of a chess piece. You have decided to make your favorite piece, the rook.</p>
+
+### 입력 
+
+ Empty
+
+### 출력 
+
+ <p>The rook art, exactly as shown below, with no extra blank spaces. In particular, a line must not end with a blank space.</p>
+
---
+++ 백준/Bronze/2393. Rook/Rook.cob
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. IDSAMPLE.
+       ENVIRONMENT DIVISION.
+       PROCEDURE DIVISION.
+       DISPLAY '  ___  ___  ___'
+       DISPLAY '  | |__| |__| |'
+       DISPLAY '  |           |'
+       DISPLAY '   \_________/'
+       DISPLAY '    \_______/'
+       DISPLAY '     |     |'
+       DISPLAY '     |     |'
+       DISPLAY '     |     |'
+       DISPLAY '     |     |'
+       DISPLAY '     |_____|'
+       DISPLAY '  __/       \__'
+       DISPLAY ' /             \'
+       DISPLAY '/_______________\'
+       STOP RUN.
