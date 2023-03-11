82282e17382b8016af2bf96aadffa566f36c8400
인준 <115498126+in-jun@users.noreply.github.com>
2023-03-12 00:01:55 +0900

    [Bronze IV] Title: 0123456789, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6916. 0123456789/0123456789.cc
@@ -0,0 +1,127 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    int input;
+    cin >> input;
+    if (input == 0)
+    {
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << '\n';
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+    }
+    else if (input == 1)
+    {
+        cout << '\n';
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << '\n';
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+    }
+    else if (input == 2)
+    {
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << " * * *\n";
+    }
+    else if (input == 3)
+    {
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+    }
+    else if (input == 4)
+    {
+        cout << '\n';
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+    }
+    else if (input == 5)
+    {
+        cout << " * * *\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+    }
+    else if (input == 6)
+    {
+        cout << " * * *\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << "*\n";
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+    }
+    else if (input == 7)
+    {
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << '\n';
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+    }
+    else if (input == 8)
+    {
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+    }
+    else if (input == 9)
+    {
+        cout << " * * *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << "*     *\n";
+        cout << " * * *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << "      *\n";
+        cout << " * * *\n";
+    }
+    return 0;
+}
