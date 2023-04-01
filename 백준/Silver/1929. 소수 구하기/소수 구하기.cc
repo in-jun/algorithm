53fb1448a7e8ab3b68ea73a0763076050383b5ba
인준 <115498126+in-jun@users.noreply.github.com>
2023-04-01 23:11:47 +0900

    [Silver III] Title: 소수 구하기, Time: 188 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+bool prime(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+        if (!(n % i))
+            return false;
+    return true;
+}
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    for (int i = m; i <= n; i++)
+    {
+        if (prime(i))
+            cout << i << '\n';
+    }
+    return 0;
+}
