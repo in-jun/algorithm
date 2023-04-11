2ebc1531634d16c8c4c512fb271fafbf419af786
in-jun <me@injun.dev>
2023-04-12 00:17:39 +0900

    [Silver I] Title: 정수 삼각형, Time: 40 ms, Memory: 2880 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -2,18 +2,20 @@
 using std::cin;
 using std::cout;
 using std::max;
+
 int main()
 {
     int n, dp[501][501]{0}, m = 0;
     cin >> n;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j <= i; j++)
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             cin >> dp[i][j];
-    for (int i = 1; i < n; i++)
-        for (int j = 0; j <= i; j++)
-            dp[i][j] += max((0 <= (j - 1)) * dp[i - 1][j - 1], dp[i - 1][j]);
-    for (int i = 0; i < n; i++)
-        m = max(m, dp[n - 1][i]);
+            dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j]);
+            m = max(m, dp[i][j]);
+        }
+    }
     cout << m;
     return 0;
 }
