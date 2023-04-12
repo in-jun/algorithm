1f7d8589546043e5489ec9594541da0847ddf098
인준 <115498126+in-jun@users.noreply.github.com>
2023-04-12 21:31:21 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -8,8 +8,7 @@ int main()
     cin >> n >> k;
     int arr[n];
     for (int i = 0; i < n; i++)
-        cin >>
-            arr[i];
+        cin >> arr[i];
     for (int i = n - 1; i >= 0; i--)
     {
         if (arr[i] <= k)
