48edb42d5e5909a169f5ed3843c67f3c6ed84afa
in-jun <me@injun.dev>
2023-04-15 20:58:00 +0900

    [Gold III] Title: 나머지 합, Time: 136 ms, Memory: 9720 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long int n, m, count = 0;
+    long int arr1[1000001]{0};
+    long int arr2[1001]{0};
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arr1[i];
+        arr1[i] += arr1[i - 1];
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        arr1[i] %= m;
+        count += !arr1[i];
+        arr2[arr1[i]]++;
+    }
+    for (int i = 0; i < m; i++)
+        count += !!arr2[i] * (arr2[i] * (arr2[i] - 1) / 2);
+    cout << count;
+    return 0;
+}
