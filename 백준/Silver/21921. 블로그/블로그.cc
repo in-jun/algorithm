1fde3a6dfa4b83f64bda48069fda08045b19021c
in-jun <me@injun.dev>
2023-05-04 18:53:13 +0900

    [Silver III] Title: 블로그, Time: 96 ms, Memory: 2876 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::max;
+int main()
+{
+    int x, n, input, Max = 0, count = 0, arr[250001]{0};
+    cin >> x >> n;
+    for (int i = 1; i <= x; i++)
+    {
+        cin >> input;
+        arr[i] += arr[i - 1] + input;
+    }
+    for (int i = n; i <= x; i++)
+        Max = max(Max, arr[i] - arr[i - n]);
+    for (int i = n; i <= x; i++)
+        count += (Max == (arr[i] - arr[i - n]));
+    if (!Max)
+        cout << "SAD";
+    else
+        cout << Max << ' ' << count;
+    return 0;
+}
