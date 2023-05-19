3bdaa43a0a515c1a2108264ee596a11050a14992
인준 <115498126+in-jun@users.noreply.github.com>
2023-05-20 02:09:16 +0900

    [Silver IV] Title: 암기왕, Time: 1728 ms, Memory: 49016 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <set>
+using std::cin;
+using std::cout;
+using std::set;
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		set<int> s;
+		int n, m, input;
+		cin >> n;
+		while (n--)
+		{
+			cin >> input;
+			s.insert(input);
+		}
+		cin >> m;
+		while (m--)
+		{
+			cin >> input;
+			cout << (s.find(input) != s.end()) << '\n';
+		}
+	}
+	return 0;
+}
