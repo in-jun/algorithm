1d37edf357489e03d353665d8d5ec91b41aab82f
in-jun <me@injun.dev>
2023-11-19 18:03:01 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cpp
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int solution(int n)
 {
     int answer = 0;
-    answer = (n % 2 ? (n / 2 + 1) * (n / 2 + 1) : (n * n * n + 3 * n * n + 2 * n) / 6);
+    answer = (n % 2 ? (n / 2 + 1) * (n / 2 + 1) : (n * (n + 1) * (n + 2)) / 6);
     return answer;
 }
