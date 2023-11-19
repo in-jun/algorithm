38cd4db6141216fdc905eee750d9513cb18521d2
인준 <h12261226@outlook.kr>
2023-11-19 12:39:00 +0900

    [level unrated] Title: 덧셈식 출력하기, Time: 1.56 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a << " + " << b << " = " << a + b << endl;
+    return 0;
+}
