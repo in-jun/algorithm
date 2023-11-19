f96186f5ead38d5832f54db4aa710f928bcfbddc
인준 <h12261226@outlook.kr>
2023-11-19 23:11:35 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.17 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -3,12 +3,7 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> arr, vector<vector<int>> queries)
-{
-    vector<int> answer = arr;
-
-    for (vector<int> s : queries)
-        swap(answer[s[0]], answer[s[1]]);
-
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer;
     return answer;
 }
