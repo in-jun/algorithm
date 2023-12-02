9d2469c06664918138f801a4fb159990ee2583a5
in-jun <me@injun.dev>
2023-12-02 19:31:51 +0900

    [level unrated] Title: 뒤에서 5등 위로, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list)
+{
+    vector<int> answer;
+
+    sort(num_list.begin(), num_list.end());
+
+    for (int i = 5; i < num_list.size(); i++)
+        answer.push_back(num_list[i]);
+
+    return answer;
+}
