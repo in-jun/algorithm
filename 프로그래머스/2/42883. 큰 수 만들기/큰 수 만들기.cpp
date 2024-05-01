a4bc8bfd9714d523199c2229b3442053198a50c0
인준 <h12261226@outlook.kr>
2024-05-01 19:11:56 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,20 +1,32 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
 string solution(string number, int k)
 {
-    string answer = number;
-    for (int i = 0; i < answer.size() - 1; i++)
+    string answer = "";
+    answer = number.substr(k);
+
+    for (int i = k - 1; i >= 0; i--)
     {
-        if ((answer[i] < answer[i + 1]) && k)
+        int j = 0;
+
+        while (1)
         {
-            answer.erase(i, 1);
-            k--;
-            i = -1;
+            if (number[i] >= answer[j])
+            {
+                char temp = answer[j];
+                answer[j] = number[i];
+                number[i] = temp;
+                j++;
+            }
+            else
+            {
+                break;
+            }
         }
     }
 
-    return answer.substr(0, number.size() - k);
+    return answer;
 }
