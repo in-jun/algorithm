77da4bed3a3b62fc6306507768d49278a1439d6e
인준 <h12261226@outlook.kr>
2024-05-30 16:18:10 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 124 ms, Memory: 3368 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using std::cin;
+using std::cout;
+using std::max;
+using std::min;
+using std::sort;
+using std::tuple;
+using std::vector;
+
+vector<tuple<int, int, int>> edges;
+vector<int> u;
+vector<int> arr;
+
+int find_root(int x)
+{
+    if (u[x] == x)
+    {
+        return x;
+    }
+
+    return u[x] = find_root(u[x]);
+}
+
+void union_root(int x, int y)
+{
+    x = find_root(x);
+    y = find_root(y);
+
+    if (x == y)
+    {
+        return;
+    }
+
+    u[max(x, y)] = min(x, y);
+}
+
+int main()
+{
+    int v, e;
+    cin >> v >> e;
+
+    u.resize(v + 1);
+    edges.resize(e + 1);
+
+    for (int i = 1; i <= v; i++)
+    {
+        u[i] = i;
+    }
+
+    for (int i = 1; i <= e; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edges[i] = {c, a, b};
+    }
+
+    edges[0] = {-1e9, 0, 0};
+
+    sort(edges.begin(), edges.end());
+
+    for (int i = 1; i <= e; i++)
+    {
+        auto [c, f, s] = edges[i];
+
+        if (find_root(f) == find_root(s))
+        {
+            continue;
+        }
+
+        arr.push_back(c);
+
+        union_root(f, s);
+
+        if (arr.size() == v - 1)
+        {
+            break;
+        }
+    }
+
+    int count = 0;
+    for (int n : arr)
+    {
+        count += n;
+    }
+
+    cout << count;
+    return 0;
+}
